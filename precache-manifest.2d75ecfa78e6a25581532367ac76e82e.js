52b6a37f4e2a553ae76372d2b5bbd9457d623b84
unknown <bagrahari21@gmial.com>
2020-05-10 09:30:08 -0700

    Updates

---
+++ precache-manifest.2d75ecfa78e6a25581532367ac76e82e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "364f34e5c6fa49b1e51d85871dd6ed75",
+    "url": "/Covid-19/index.html"
+  },
+  {
+    "revision": "c8c0dee8f9abbc4e459f",
+    "url": "/Covid-19/static/css/main.f51b326b.chunk.css"
+  },
+  {
+    "revision": "aa5e701ead300076b239",
+    "url": "/Covid-19/static/js/2.433928ee.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Covid-19/static/js/2.433928ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8c0dee8f9abbc4e459f",
+    "url": "/Covid-19/static/js/main.60d0b5cd.chunk.js"
+  },
+  {
+    "revision": "c66d3331efbb4a716459",
+    "url": "/Covid-19/static/js/runtime-main.a1321883.js"
+  },
+  {
+    "revision": "3d7ddb3fa66cf89ece74ea146548b9e3",
+    "url": "/Covid-19/static/media/covid19.3d7ddb3f.png"
+  }
+]);
