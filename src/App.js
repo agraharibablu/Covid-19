f37d5cc5ae43adfa4b5616afa9ee1e922ca2b2ce
unknown <bagrahari21@gmial.com>
2020-05-10 07:04:24 -0700

    Test React App

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import styles from './App.module.css';
+import { Cards, Chart, CountryPicker } from './components';
+
+import coronaImage from './images/covid19.png';
+
+import { fetchData } from './api';
+ 
+class App extends React.Component {
+
+state = {
+    data: {},
+    country: '',
+}
+
+async componentDidMount(){
+
+    const fetchdata = await fetchData();
+
+this.setState({data: fetchdata });
+}
+
+handleCountryChange = async (country) => {
+   
+    const fetchedData = await fetchData(country);
+    // fetch the data
+
+    // set the state
+    //console.log(fetchedData);
+    //console.log(country);
+    this.setState({data: fetchedData, country: country });
+}
+
+    render(){
+        const { data , country} = this.state;
+        return(
+          
+        <div className={styles.container}>
+            <img className={styles.image} src={coronaImage} alt='COVID-19' />
+            <Cards data={data} />
+            <CountryPicker handleCountryChange={this.handleCountryChange} />
+            <Chart data={data} country={country} />
+        </div>
+        
+        );
+    }
+}
+export default App;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+//import Cards from './components/Cards/Cards';
+///import Cards from './components/Chart/Chart';
+//import Cards from './components/CountryPicker/CountryPicker';
+
+
+
+ReactDOM.render(<App />,document.getElementById('root'));
